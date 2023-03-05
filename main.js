b9dca5921087c95400128a60c5f7e781deff6a36
Jordy <jbonn2002@gmail.com>
2023-03-04 17:04:59 -0800

    First Commit of second three.js project

---
+++ main.js
@@ -0,0 +1,54 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { BoxLineGeometry } from 'three/addons/geometries/BoxLineGeometry.js';
+import { Color } from 'three';
+
+
+let container;
+let camera, scene, renderer;
+
+let room;
+
+init();
+animate();
+
+function init() {
+
+  container = document.createElement( 'div' );
+  document.body.appendChild( container );
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0x000000 );
+
+  camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 10 );
+  camera.position.set( 0, 1.6, 3 );
+  scene.add( camera );
+
+  room = new THREE.LineSegments(
+    new BoxLineGeometry( 6, 6, 6, 10, 10, 10 ).translate( 0, 3, 0 ),
+    new THREE.LineBasicMaterial( { color: 0x808080 } )
+  );
+  scene.add( room );
+
+  scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
+
+  const light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 1, 1, 1 ).normalize();
+  scene.add( light );
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.xr.enabled = true;
+  container.appendChild( renderer.domElement );
+
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera)
+}
